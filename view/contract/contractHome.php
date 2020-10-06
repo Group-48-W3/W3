5a62bc3f9385fdb3897fb48ef1a77fcb640a469f
SahanDisa <t.sahan998@gmail.com>
2020-10-06 12:36:41 +0530

    Update contractHome.php

--- view/contract/contractHome.php
+++ view/contract/contractHome.php
@@ -46,6 +46,21 @@
         <button type="button" class="close" data-dismiss="alert">&times;</button>
         <strong>Well done!</strong> You successfully read <a href="#" class="alert-link">this important alert message</a>.
     </div>
-    
+    <!-- 1st card -->
+    <div class="card text-white bg-primary mb-3" style="max-width: 20rem;">
+      <div class="card-header">Header</div>
+      <div class="card-body">
+        <h4 class="card-title">Primary card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
+    <!--2nd card  -->
+    <div class="card border-primary mb-3" style="max-width: 20rem;">
+      <div class="card-header">Header</div>
+      <div class="card-body">
+        <h4 class="card-title">Primary card title</h4>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      </div>
+    </div>
 </body>
 </html>
