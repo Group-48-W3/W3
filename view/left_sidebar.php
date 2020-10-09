877247f842c6212d9e883be90fdd77b174994938
SahanDisa <t.sahan998@gmail.com>
2020-10-09 20:48:02 +0530

    Import Calculator

--- view/left_sidebar.php
+++ view/left_sidebar.php
@@ -64,7 +64,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a href="#" class="nav-link">
+        <a href="./expense/expenseHome.php" class="nav-link">
           <svg 
             aria-hidden="true" 
             focusable="false" 
