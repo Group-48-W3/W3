fe1cd3de248e5149714bda899b0575c5eb0e5cc1
SahanDisa <t.sahan998@gmail.com>
2020-10-09 14:16:45 +0530

    Responsive Measures

--- view/contract/contractHeader.php
+++ view/contract/contractHeader.php
@@ -9,9 +9,9 @@
  <!--Navbar starts -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="../dashboard.php">W3 DASHBOARD</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
 
   <div class="collapse navbar-collapse" id="navbarColor02">
     <ul class="navbar-nav mr-auto">
