d8638401ad7a40d0edf2619b964e475f4c15f67d
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2020-10-15 20:45:58 +0530

    Update main.js
    Add tab opener

--- assets/js/scripts/main.js
+++ assets/js/scripts/main.js
@@ -1,36 +1,13 @@
-function expandContractMenu() {
-    var x = document.getElementById("contracts-menu");
-    if(window.innerWidth>600) {
-        if (x.style.display === "none") {
-            x.style.display = "flex";
-            document.getElementById("contract-drop").style.transform = "rotate(180deg)";
-        } else {
-            x.style.display = "none";
-            document.getElementById("contract-drop").style.transform = "rotate(0deg)";
-        }
-    }else{
-        if (x.style.display === "none") {
-            //scroll up menu
-        } else {
-            //scroll down menu
-        }
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
     }
-}
-function expandReportMenu() {
-    var x = document.getElementById("reports-menu");
-    if(window.innerWidth>600) {
-        if (x.style.display === "none") {
-            x.style.display = "flex";
-            document.getElementById("report-drop").style.transform = "rotate(180deg)";
-        } else {
-            x.style.display = "none";
-            document.getElementById("report-drop").style.transform = "rotate(0deg)";
-        }
-    }else{
-        if (x.style.display === "none") {
-            //scroll up menu
-        } else {
-            //scroll down menu
-        }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-}
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
