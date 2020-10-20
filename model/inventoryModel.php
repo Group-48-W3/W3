abd911da81b1d57869a0d678bd634f1b90a6da81
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2020-10-21 02:06:53 +0530

    model insert interfaces

--- model/inventoryModel.php
+++ model/inventoryModel.php
@@ -1,6 +1,18 @@
 <?php
-require_once("./../../inc/config.php");
+    require_once("./../../inc/config.php");
 
+    //insert commands
+    function insertToRawMaterial($materialName, $materialType, $materialPrice, $materialQuantity, $materialReorderValue){
+        global $conn;
+        //insert to database
+    }
 
+    function insertToTool($toolName, $toolPrice, $toolManufacturer, $toolQuantity, $toolReorderValue){
+        global $conn;
+    }
+
+    function insertToMaintenance(){
+        global $conn;
+    }
 
 ?>
