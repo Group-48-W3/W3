fc0e53f431d8c296e0b360708984dc0906dd4d75
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2020-10-21 02:10:01 +0530

    impleent model for maintenance checking

--- model/inventoryModel.php
+++ model/inventoryModel.php
@@ -43,6 +43,20 @@
         }
     }
 
+    function isInMaintenance($toolId){
+        global $conn;
+        $sql = "select * from maintenance where tool_id = '".$toolId."'";
+				
+	    $result = mysqli_query($conn, $sql);
+        $numRows = mysqli_num_rows($result);
+
+        if($numRows == 0){
+            return 0;
+        }else{
+            return 1;
+        }
+    }
+
     //read commands
     function selectFromRawMaterial(){
         global $conn;
