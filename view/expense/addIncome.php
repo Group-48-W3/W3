17381a90874045213f5f316d98613efe18b2236e
ShanukaNF <56572468+ShanukaNF@users.noreply.github.com>
2020-11-29 17:17:16 +0530

    Update addIncome.php

--- view/expense/addIncome.php
+++ view/expense/addIncome.php
@@ -1,5 +1,10 @@
 <?php
   session_start();
+  if(!isset($_SESSION['u_id'],$_SESSION['r_id']))
+	{
+		header('location:index.php?lmsg=true');
+		exit;
+	}
   require_once('../../controller/user/userController.php');
   include_once('header.php'); 
 ?>
