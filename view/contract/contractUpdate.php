b85612059b8529c37f61aa71556e31aa56e2b761
SahanDisa <t.sahan998@gmail.com>
2020-12-07 12:01:14 +0530

    Add: Backend Sync with Contract Update

--- view/contract/contractUpdate.php
+++ view/contract/contractUpdate.php
@@ -6,7 +6,20 @@
     exit;
   }		
  require_once('./../../controller/user/userController.php'); 
+ require_once('./../../controller/contract/contractController.php'); 
  require_once('./header.php');
+
+ if (isset($_GET['con_id'])){
+    $this_contract = $_GET['con_id'];
+    $con = new Contract();
+    $con_details = $con->getSingleActiveContract($_SESSION['contract_id']);
+    
+    $row = mysqli_fetch_array($con_details);
+    
+    $client_details = $con->getSingleClient($row['c_id']);
+
+    $row_client = mysqli_fetch_array($client_details);
+ }
  ?>
 
 <div class="container">
@@ -16,57 +29,58 @@
     <h4>Step 01 : Update Contract Details</h4>
       <form method="post" action="./../../controller/contract/contractController.php">
         <div class="form-group field">
-          <input type="text" class="form-field" name="con_name" id="con_name" required>
+          <input type="text" class="form-field" name="con_name" id="con_name" value="<?php echo $row['con_name'];?>" required>
           <label for="con_name" class="form-label">Contract Name</label>
         </div>
         <div class="form-group field">
-          <input type="date" class="form-field" name="con_start_date" id="con_start_date" required>
+          <input type="date" class="form-field" name="con_start_date" id="con_start_date" value="<?php echo $row['startdate'];?>" required>
           <label for="con_start_date" class="form-label">Start Date</label>
         </div>
         <div class="form-group field">
-          <input type="date" class="form-field" name="con_end_date" id="con_end_date" required>
+          <input type="date" class="form-field" name="con_end_date" id="con_end_date" value="<?php echo $row['enddate'];?>" required>
           <label for="con_end_date" class="form-label">End Date</label>
         </div>
         <div class="form-group field">
-          <input type="text" class="form-field" name="con_location" id="con_location" required>
+          <input type="text" class="form-field" name="con_location" id="con_location" value="<?php echo $row['location'];?>" required>
           <label for="con_location" class="form-label">Location</label>
           <small id="" class="form-text text-muted">Provide the location by nearest main town (Location eg:- Colombo 7)</small>
         </div>
         <div class="form-group field">
-          <input type="text" class="form-field" name="con_description" id="con_description" required>
+          <input type="text" class="form-field" name="con_description" id="con_description" value="<?php echo $row['con_desc'];?>" required>
           <label for="con_description" class="form-label">Description</label>
         </div>
         
         <div class="form-group field">
-          <input type="text" class="form-field" name="con_payment" id="con_payment" required>
+          <input type="text" class="form-field" name="con_payment" id="con_payment" value="<?php echo $row['payment_method'];?>" required>
           <label for="con_payment" class="form-label">Payment Method</label>
         </div>
         
-          <!-- Step 02 -->
+        <!-- Step 02 -->
         <h4>Step 02 : Update Client Details</h4>
         <div class="form-group field">
-          <input type="text" class="form-field" name="c_name" id="c_name">
+          <input type="text" class="form-field" name="c_name" id="c_name" value="<?php echo $row_client['c_name'];?>">
           <label for="c_name" class="form-label">Client Name</label>
           <small id="" class="form-text text-muted">Give a fullname name for your client in lower case (eg:- nipun fernando)</small>
         </div>
         <div class="form-group field">
-          <input type="text" class="form-field" name="c_address" id="c_address">
+          <input type="text" class="form-field" name="c_address" id="c_address" value="<?php echo $row_client['c_address'];?>" >
           <label for="c_address" class="form-label">Client Address</label>
           <small id="" class="form-text text-muted">eg:- Reid Avenue, Colombo 7</small>
         </div>
         <div class="form-group field">
-          <input type="text" class="form-field" name="c_company" id="c_company">
+          <input type="text" class="form-field" name="c_company" id="c_company" value="<?php echo $row_client['c_company'];?>">
           <label for="c_company" class="form-label">Client Company</label>
         </div>
         <div class="form-group field">
-          <input type="text" class="form-field" name="c_mobile" id="c_mobile">
+          <input type="text" class="form-field" name="c_mobile" id="c_mobile" value="<?php echo $row_client['c_mobile'];?>">
           <label for="c_mobile" class="form-label">Client Mobile</label>
           <small class="form-text text-muted">provide mobile number +94 notation (eg:- +94123456789)</small>
         </div>
         <div class="form-group field">
-          <input type="text" class="form-field" name="c_email" id="c_email">
+          <input type="text" class="form-field" name="c_email" id="c_email" value="<?php echo $row_client['c_email'];?>">
           <label for="c_email" class="form-label">Client Email</label>
         </div> 
+        <!-- Update Button to update the contract details -->
         <input type="submit" class="btn btn-warning" name="contractUpdate" value="Update Contract">
       </form>
 </div>
