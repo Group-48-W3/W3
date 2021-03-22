0f90d87408992634b0ca28f15858868e86707643
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-03-22 14:42:22 +0530

    Update: Progress on ContractSinglePage

--- view/contract/contractSinglePage.php
+++ view/contract/contractSinglePage.php
@@ -77,15 +77,28 @@
   <div class="col-sm">
     <!-- Progress starts -->
     <h2>Progress Measures</h2>
-    <div class="circles">
-      <h5>Progess : <?php echo " ".$progress." %"?></h5>
-      
+    <div class="col-sm">
+      <h5>Progress : <?php echo " ".$progress." %"?></h5>
       <br>
-      <div class="third circle">
-        <strong></strong>
-        <!-- <span><br><?php echo $row["con_name"]." "; ?>progress</span> -->
-        
-      </div>
+      <!-- add progress bar -->
+      <?php if($progress >=50){ ?>
+        <svg class="radial-progress" data-percentage="<?php echo $progress;?>" viewBox="0 0 80 80">
+          <circle class="incomplete" cx="40" cy="40" r="35"></circle>
+          <circle class="complete" cx="40" cy="40" r="35" style="stroke-dashoffset: 39.58406743523136;"></circle>
+          <text class="percentage" x="50%" y="57%" transform="matrix(0, 1, -1, 0, 80, 0)"><?php echo $progress?></text>
+        </svg>
+      <?php }else{?>
+        <!-- progress less than 50% -->
+        <svg class="radial-progress" data-percentage="<?php echo $progress;?>" viewBox="0 0 80 80">
+          <circle class="incomplete" cx="40" cy="40" r="35"></circle>
+          <circle class="complete" cx="40" cy="40" r="35" style="stroke-dashoffset: 39.58406743523136;"></circle>
+          <text class="percentage" x="50%" y="57%" transform="matrix(0, 1, -1, 0, 80, 0)"><?php echo $progress?></text>
+        </svg>  
+      <?php }?>  
+      <!-- progress with js  -->
+      
+      <!-- ends -->
+    </div>
     </div>
     <!-- Progress ends -->
   </div>
@@ -386,6 +399,32 @@
             $pager.insertAfter($table).find('span.page-number:first').addClass('active');
         }
     });
+</script>
+<!-- script for progress -->
+<script>
+$('svg.radial-progress').each(function( index, value ) { 
+  $(this).find($('circle.complete')).removeAttr( 'style' );
+});
+$(window).scroll(function(){
+  $('svg.radial-progress').each(function( index, value ) { 
+    // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+    if ( 
+        $(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+        $(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+    ) {
+        // Get percentage of progress
+        percent = $(value).data('percentage');
+        // Get radius of the svg's circle.complete
+        radius = $(this).find($('circle.complete')).attr('r');
+        // Get circumference (2πr)
+        circumference = 2 * Math.PI * radius;
+        // Get stroke-dashoffset value based on the percentage of the circumference
+        strokeDashOffset = circumference - ((percent * circumference) / 100);
+        // Transition progress for 1.25 seconds
+        $(this).find($('circle.complete')).animate({'stroke-dashoffset': strokeDashOffset}, 1250);
+    }
+  });
+}).trigger('scroll');
 </script>
 <?php 
  require_once('leftSidebar.php'); 
