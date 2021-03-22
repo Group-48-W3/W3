e74c3239f14cc1b1d0d7c6a9281610da573a492c
Sahan Dissanayaka <tsahandisaai@gmail.com>
2021-03-22 14:54:44 +0530

    Fix: Flexibility to View and Update in Single Page

--- view/contract/contractSinglePage.php
+++ view/contract/contractSinglePage.php
@@ -160,7 +160,7 @@
                     <td data-label="Name"><?php echo $row["q_name"]; ?></td>
                     <td data-label="Name">
                     <!-- <a onclick="document.getElementById('item').style.display='block'"><?php echo $row["q_item"]; ?></a> -->
-                    <a href="./itemUpdate.php?item_id=<?php echo $row["q_item"]; ?>"><?php echo $row["q_item"]; ?></a>
+                    <a href="./itemUpdate.php?view=1&item_id=<?php echo $row["q_item"]; ?>"><?php echo $row["q_item"]; ?></a>
                     </td>
                     <td data-label="Description"><?php echo $row["q_desc"]; ?></td>
                     <td data-label="Budget"><?php echo $row["q_budget"];?></td>
