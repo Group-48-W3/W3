a027bc798d55dff8e6edc0b228cf8663346a46cb
Udara Weerasinghe <64691719+udara-kw@users.noreply.github.com>
2021-03-25 08:06:00 +0530

    update grn

--- view/inventory/goodRecieveNote.php
+++ view/inventory/goodRecieveNote.php
@@ -1,23 +1,132 @@
-<?php 
-	session_start();
-	
-	if(!isset($_SESSION['u_id'],$_SESSION['r_id'])){
-		header('location:index.php?lmsg=true');
-		exit;
-	}		
-	
-  require_once('../../controller/user/userController.php');
-  require_once('header.php');
+<?php
+session_start();
+
+if (!isset($_SESSION['u_id'], $_SESSION['r_id'])) {
+  header('location:index.php?lmsg=true');
+  exit;
+}
+
+require_once('../../controller/user/userController.php');
+require_once('../../controller/inventory/rawMaterialController.php');
+require_once('../../controller/inventory/supplierController.php');
+$rawMaterial = new RawMaterial();
+$supplier = new Supplier();
+require_once('header.php');
+
+$material = $_SESSION['replenishMaterialId'];
+$amount = $_SESSION['replenishMaterialAmount'];
+$unitPrice = $_SESSION['replenishUnitPrice'];
+$supplierID = $_SESSION['replenishSupplier'];
+$expire = $_SESSION['replenishPeriod'];
+$delivery = $_SESSION['replenishDelivery'];
+
+$supplierResult = $supplier->getSupplier($supplierID);
+$supplierDetails = mysqli_fetch_array($supplierResult);
 ?>
 
-<h2>Good Recieve Note</h2>
-<div class="container center">
-  <img src="../../public/img/goodRecieveNote.png" alt="" width="800"><br>
+<div class="container center" style="border: 1px solid; padding-right:2.5rem;">
+  <p>
+    <i>W3 Contracts - Willorawatte, Moratuwa</i>
+    <br><i>011-2265748 / 071-2554698</i>
+  </p>
+  <h1>Good Recieve Note</h1>
+
+  <div class="container">
+    <hr>
+    <h2 class="left">Supplier Details</h2>
+    <div class="row">
+      <div class="col">
+        <div class="left">
+          <p>Supplier : <i><?php echo $supplierDetails['sup-name'] ?></i></p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="right">
+          <p>Address : <i><?php echo $supplierDetails['sup-address'] ?></i></p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="left">
+          <p>Email : <i><?php echo $supplierDetails['sup-email'] ?></i></p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="right">
+          <p>Contact : <i><?php echo $supplierDetails['sup-mobile'] ?></i></p>
+        </div>
+      </div>
+    </div>
+    <hr>
+    <h2 class="left">Item Details</h2>
+    <div class="container" style="width:75%;">
+      <div class="row">
+
+        <table>
+          <thead>
+            <th>Item</th>
+            <th>Quantity</th>
+            <th>Expiry Date</th>
+            <th>Unit Price</th>
+            <th>Total</th>
+            <th>Comment</th>
+          </thead>
+          <tbody>
+            <tr>
+              <td><?php echo $material ?></td>
+              <td><?php echo $amount ?></td>
+              <td><?php echo $expire ?></td>
+              <td><?php echo "Rs. " . $unitPrice ?></td>
+              <td><?php echo "Rs. " . $unitPrice * $amount ?></td>
+              <td>Received</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+    <br>
+    <hr>
+    <h2 class="left">Delivery Details</h2>
+    <div class="row">
+      <div class="col">
+        <div class="left">
+          <p>Delivered by : <i><?php echo $delivery ?></i></p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="right">
+          <p>Date : <i><?php echo date('Y-m-d'); ?></i></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <br>
+  <div class="container">
+    <hr>
+    <h2 class="left">Signature</h2>
+    <div class="row">
+      <div class="col">
+        <div class="left">
+          <p><i><u>Rahal Weerarathne</u></i><br>Owner<br>W3 Contracts, Willorawatta</p>
+        </div>
+      </div>
+      <div class="col">
+        <div class="right">
+          <p>Date : <i><?php echo date('Y-m-d'); ?></i></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!--<img src="../../public/img/goodRecieveNote.png" alt="" width="800"><br>-->
+
+</div>
+<br>
+<div class="center">
   <button class="btn btn-primary">Send as email</button>
   <button class="btn btn-primary">Download PDF</button>
 </div>
-
 <?php
-  require_once('leftSidebar.php'); 
-  require_once('footer.php'); 
-?>	
+require_once('leftSidebar.php');
+require_once('footer.php');
+?>
